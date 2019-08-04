89e978a23ebadb8c8a1a730760787cb7b151bb78
Raghda93 <38849080+Raghda93@users.noreply.github.com>
2019-08-04 08:44:28 +0300

    Add files via upload

---
+++ wikiMedia2/src/test/java/com/article/utility/UtilitiesTest.java
@@ -0,0 +1,60 @@
+package main.java.com.article.utility;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+
+class UtilitiesTest {
+
+	@Test
+	void testIsEmptyString() {
+		String str = "Hello";
+		
+		boolean idEmpty = Utilities.isEmptyString(str);
+		assertFalse(idEmpty);
+		
+		idEmpty = Utilities.isEmptyString(null);
+		assertTrue(idEmpty);
+		
+		idEmpty = Utilities.isEmptyString("");
+		assertTrue(idEmpty);
+	}
+
+	@Test
+	void testIsEmptyCollection() {
+		ArrayList<Object> arrayList = null;
+		assertTrue(Utilities.isEmptyCollection(arrayList));
+		
+		arrayList = new ArrayList<>();
+		assertTrue(Utilities.isEmptyCollection(arrayList));
+		
+		arrayList.add("Hello");
+		assertFalse(Utilities.isEmptyCollection(arrayList));
+		
+	}
+
+	@Test
+	void testIsEmptyMap() {
+		HashMap<Object, Object> map = null;
+		assertTrue(Utilities.isEmptyMap(map));
+		
+		map = new HashMap<>();
+		assertTrue(Utilities.isEmptyMap(map));
+		
+		map.put(1, "Hello");
+		assertFalse(Utilities.isEmptyMap(map));
+		
+	}
+
+	@Test
+	void testEscapeMetaCharacters() {
+		String str = "\\ \" Hello World";
+
+		String actual = Utilities.escapeMetaCharacters(str);
+		assertEquals("'\\ '\" Hello World", actual);
+	}
+
+}
